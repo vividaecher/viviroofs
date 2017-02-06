b0b442cdd8ca987144e79666b203282258b5bc8e
Virginia Dächer <daecher@sipgate.de>
2017-02-06 10:30:53 +0100

    adds social media navigation

--- js/init.js
+++ js/init.js
@@ -8,21 +8,3 @@ $(".navbar-toggle").on("click", function() {
 /*$(".partei-radio").on("click", function() {
   $(".submit button").removeAttr("disabled");
 });*/
-
-document.ontouchmove = function ( event ) {
-
-    var isTouchMoveAllowed = true, target = event.target;
-
-    while ( target !== null ) {
-        if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
-            isTouchMoveAllowed = false;
-            break;
-        }
-        target = target.parentNode;
-    }
-
-    if ( !isTouchMoveAllowed ) {
-        event.preventDefault();
-    }
-
-};
