d746494073c9ffd4ac67a1ff77c6ede4a477a7bf
Virginia Dächer <daecher@sipgate.de>
2017-02-13 13:46:38 +0100

    changes color of navbar-toggle if scrolling

--- js/init.js
+++ js/init.js
@@ -4,3 +4,29 @@ $(".navbar-toggle").on("click", function() {
   $(this).toggleClass("close-navigation");
   $("body").toggleClass("navigation-open");
 });
+
+$(window).scroll(function(e){
+  var amount = $(window).scrollTop();
+
+  if( amount > 100 ){
+    $('.logo').addClass('color-logo');
+  }else {
+    $('.logo').removeClass('color-logo');
+  }
+
+  if( amount > 100 ){
+    $('.navbar-toggle').addClass('color-navbar');
+  }else {
+    $('.navbar-toggle').removeClass('color-navbar');
+  }
+
+  if( amount > 100 ){
+    $('.navbar-toggle span').addClass('color-span');
+  }else {
+    $('.navbar-toggle span').removeClass('color-span');
+  }
+});
+
+$(document).ready(function(){
+    $('.chocolat-parent').Chocolat();
+});
